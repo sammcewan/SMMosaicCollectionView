1ce5bf4b9b8937d08ce867100eb9138958615006
Sam McEwan <me@sammcewan.co.nz>
2014-04-21 16:37:22 +1200

    Initial Commit

---
+++ SMMosaicCollectionView/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  SMMosaicCollectionView
+//
+//  Created by Sam McEwan on 21/04/14.
+//  Copyright (c) 2014 Sam McEwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SMMosaicCollectionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SMMosaicCollectionView
+//
+//  Created by Sam McEwan on 21/04/14.
+//  Copyright (c) 2014 Sam McEwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+  }
+}
